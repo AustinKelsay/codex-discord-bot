9521d89ec53e14c89efcbc867e08a5e538bbf7ed
Austin Kelsay <53542748+AustinKelsay@users.noreply.github.com>
2022-12-05 23:08:47 +0000

    MVP

--- codex.py
+++ codex.py
@@ -4,13 +4,18 @@ import openai
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
 def ask(q):
+  # write each question to a txt file
+  with open("questions.txt", "a") as f:
+        f.write(q + "\n")
+
   response = openai.Completion.create(
         engine="code-davinci-002",
-        prompt=f"\"\"\"\n{q}\n\"\"\"",
+        prompt=f"/*{q}*/",
+        stop="/",
         temperature=0,
-        max_tokens=3500,
+        max_tokens=1000,
         top_p=1,
-        frequency_penalty=0,
-        presence_penalty=0)
+        frequency_penalty=0.3,
+        presence_penalty=0.3)
 
   return response.choices[0].text
--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 import discord
 import codex
 import os
+import dotenv
 import keep_alive
 
 intents = discord.Intents.default()
 intents.members = True
 client = discord.Client(intents=intents)
 
+dotenv.load_dotenv()
+
 BOT_TOKEN = os.getenv("BOT_TOKEN")
+
 
 @client.event
 async def on_ready():
@@ -16,21 +20,16 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    # make sure the bot doesn't reply to itself
-    if message.author == client.user:
-        return
-    # make sure this is a dm
-    if message.guild is not None:
-        return
-    # make sure the message is not empty
-    if message.content == "":
-        return
-    # make sure the message is not a command
-    if message.content.startswith("!"):
-        return
-    answer = codex.ask(message.content)
-    if answer:
-        await message.channel.send(answer)
+    # Check if the message was sent in the correct channel
+    if message.channel.id == 1049433960905711646:
+        # Check if the bot was mentioned in the message
+        if client.user in message.mentions:
+            # Generate a response using the codex.ask method
+            answer = codex.ask(message.content)
+
+            if answer:
+                # Send the response to the channel
+                await message.channel.send(answer)
 
 keep_alive.keep_alive()
 client.run(BOT_TOKEN)
